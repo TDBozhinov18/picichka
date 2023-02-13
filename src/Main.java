2ad462db4a42216bd62880557457e0119de1f16c
TDBozhinov18 <TDBozhinov18@codingburgas.bg>
2023-02-13 14:41:01 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Pizza> pizzas = new ArrayList<>();
+        ArrayList<Topping> toppings = new ArrayList<>();
+
+    }
+    public static boolean isCorrectDoughUsed(Pizza pizza, String name) {
+        return pizza.getDought().equalsIgnoreCase(name);
     }
 
-    
-
-    public static void printAllInfo(ArrayList<Pizza> pizzas) {
-        for (Pizza h : pizzas) {
-            System.out.println(h.getName().toString());
+    public static boolean isCorrectToppingInPizzaAsInList(Pizza pizza, ArrayList<Topping> toppings) {
+        for (Topping topping: toppings) {
+            if (topping.equals(pizza.getTopping()))
+                return true;
         }
+        return false;
+    }
+
+    public static boolean isCorrectPizzaName(Pizza pizza) {
+        return (pizza.getName().trim() != "" && pizza.getName().length() > 15);
     }
 
 }
